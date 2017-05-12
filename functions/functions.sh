425ec7907a1e43cc0db9a75af354dede9e6b11e3
DeadlockState <5185967+DeadlockState@users.noreply.github.com>
2017-05-13 00:28:47 +0200

    Create functions.sh

---
+++ functions/functions.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+. functions/fix_locale.sh
+. functions/update_upgrade_system.sh
+. functions/install_plexmediaserver.sh
+. functions/install_sonarr.sh
+. functions/install_radarr.sh
+. functions/install_sickrage.sh
+. functions/couchpotato.sh
+. functions/rtorrent_rutorrent.sh
+. functions/transmission.sh
+. functions/install_jackett.sh
+. functions/install_plexpy.sh
+. functions/show_result.sh
