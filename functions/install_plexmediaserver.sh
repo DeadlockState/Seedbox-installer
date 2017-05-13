2d155c2b26dbd567f1d389bbf29e480781710979
DeadlockState <5185967+DeadlockState@users.noreply.github.com>
2017-05-13 02:50:34 +0200

    Update install_plexmediaserver.sh

--- functions/install_plexmediaserver.sh
+++ functions/install_plexmediaserver.sh
@@ -6,7 +6,7 @@ install_plexmediaserver () {
 
 	cd /tmp/
  
-	wget https://downloads.plex.tv/plex-media-server/1.5.6.3790-4613ce077/plexmediaserver_1.5.6.3790-4613ce077_amd64.deb
+	wget https://downloads.plex.tv/plex-media-server/1.5.6.3790-4613ce077/plexmediaserver_1.5.6.3790-4613ce077_amd64.deb --quiet
 	
 	dpkg -i plexmediaserver_1.5.6.3790-4613ce077_amd64.deb
 	
