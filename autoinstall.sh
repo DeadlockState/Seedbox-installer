2348111f0553cc4ca18daa3b0ebc9096ec1c6fb7
DeadlockState <5185967+DeadlockState@users.noreply.github.com>
2017-06-12 15:21:37 +0200

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,36 +1,40 @@
 #!/bin/sh
-if [ $USER = "root" ] ; then
-	if ping -c 2 google.com >> /dev/null 2>&1 ; then
-		apt-get install -y git-core > /dev/null 2>&1
-		
-		mkdir Seedbox-installer
-		
-		git clone https://github.com/Punk--Rock/Seedbox-installer.git Seedbox-installer/ > /dev/null 2>&1
-		
-		mv Seedbox-installer/* .
-		
-		rm -rf Seedbox-installer/
-		
-		rm README.md
-		
-		chmod +x Seedbox.sh
-		
-		clear
-		
-		dir=`echo "$PWD" | sed -e "s/\/root/~/g"`
-		
-		echo $USER"@"$(hostname)":"$dir"# ./Seedbox.sh"
-		
-		./Seedbox.sh
-		
-		rm autoinstall.sh && rm autoinstall.sh.1
-	else
-		echo ""
-		echo " You are not connected to internet please check your connection !"
-		echo ""
-	fi
-else
+if [ $USER != "root" ] ; then
 	echo ""
-	echo "You are not root :("
+	echo " This script must be run as root !"
 	echo ""
+	
+	exit
 fi
+
+if ! ping -c 1 google.com >> /dev/null 2>&1 ; then
+	echo ""
+	echo " You are not connected to internet please check your connection !"
+	echo ""
+	
+	exit
+fi
+
+apt-get install -y git-core > /dev/null 2>&1
+
+mkdir Seedbox-installer
+
+git clone https://github.com/Punk--Rock/Seedbox-installer.git Seedbox-installer --quiet
+
+mv Seedbox-installer/* .
+
+rm -rf Seedbox-installer/
+
+rm README.md
+
+chmod +x seedbox.sh
+
+clear
+		
+dir=`echo "$PWD" | sed -e "s/\/root/~/g"`
+
+echo $USER"@"$(hostname)":"$dir"# ./seedbox.sh"
+
+./seedbox.sh
+
+rm autoinstall.sh*
