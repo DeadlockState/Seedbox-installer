8f0b290e8377e0b8637bf5c246e39452a02d75f7
DeadlockState <5185967+DeadlockState@users.noreply.github.com>
2017-12-10 13:34:36 +0100

    Update install_plexmediaserver.sh

--- functions/install_plexmediaserver.sh
+++ functions/install_plexmediaserver.sh
@@ -6,7 +6,7 @@ install_plexmediaserver () {
 
 	cd /tmp/
  
-	wget https://downloads.plex.tv/plex-media-server/1.9.4.4325-1bf240a65/plexmediaserver_1.9.4.4325-1bf240a65_amd64.deb --quiet
+	wget https://downloads.plex.tv/plex-media-server/1.10.0.4523-648bc61d4/plexmediaserver_1.10.0.4523-648bc61d4_amd64.deb --quiet
 	
 	# Little fix for containers/LXC running on Ubuntu Server
 	mv /sbin/udevadm /sbin/udevadm.bak
@@ -16,7 +16,7 @@ install_plexmediaserver () {
 	chmod +x /sbin/udevadm
 	# Little fix for containers/LXC running on Ubuntu Server
 	
-	dpkg -i plexmediaserver_1.9.4.4325-1bf240a65_amd64.deb
+	dpkg -i plexmediaserver_1.10.0.4523-648bc61d4_amd64.deb
 	
 	# Little fix for containers/LXC running on Ubuntu Server
 	rm /sbin/udevadm
